bcaf6cf9a23b1199c9b99e0f18bf0ac215b0bb83
Kyungwook Cho <amisay5256@naver.com>
2018-09-15 16:50:41 +0900

    playerItem 로그 추가

--- playerItem.js
+++ playerItem.js
@@ -2,36 +2,38 @@ let myQuery = require('./myQuery');
 
 exports.onMessageFindPlayerItems = (ws, data) => {
     findPlayerItems(data.playerId, (res) => {
+        console.log('findPlayerItems:' + res);
         let dataObj;
         for(let i = 0; i < res.length; i++) {
-            dataObj[i] = { playerId: res[i].playerId, itemId: res[i].itemId }
+            dataObj[i] = { playerId: res[i].playerId, itemId: res[i].itemId, number: res[i].number }
         }
         let sendObj = { status: 'PlayerItem', data: JSON.stringify(dataObj) };
+        console.log(JSON.stringify(sendobj));
         ws.send(JSON.stringify(sendObj));
     });
 }
 
 exports.onMessageInsertPlayerItem = (ws, data) => {
     insertPlayerItem(data.playerId, data.itemId, data.number, (res) => {
-
+        console.log('insertPlayerItem: ' + res);
     });
 }
 
 exports.onMessageDeletePlayerItem = (ws, data) => {
     deletePlayerItem(data.playerId, data.itemId, (obj) => {
-
+        console.log('deletePlayerItem: ' + obj);
     });
 }
 
 exports.onMessageDeletePlayerItems = (ws, data) => {
     deletePlayerItems(data.playerId, (obj) => {
-
+        console.log('deletePlayerItems: ' + obj);
     });
 }
 
 exports.onMessageUpdatePlayerItem = (ws, data) => {
     updatePlayerItem(data.playerId, data.itemId, data.number, (res) => {
-
+        console.log('updatePlayerItem: res');
     });
 }
 
